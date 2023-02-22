3102e518d6455c6d3922e8a41c53c05c8d382988
Jash <jashsharma619@gmail.com>
2023-02-22 21:26:44 +0530

    Rest Api Tested

--- sit_app/views.py
+++ sit_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+@api_view(['GET'])
+def getData(request):
+    person = {
+        'name':'Dennis',
+        'age': '28'
+    }
+    return Response(person)
